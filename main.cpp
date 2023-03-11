a722c026eb331ec714e056cf98d5b7ddfa2c76bb
i-dont-have-any-good-names <67664504+i-dont-have-any-good-names@users.noreply.github.com>
2023-03-11 12:39:12 -0600

    Update main.cpp
    Better SDL Setup

--- main.cpp
+++ main.cpp
@@ -1,45 +1,34 @@
+//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <SDL_image.h>
 
-//Screen dimension
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
+//Screen dimension constants
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
 
-int main(int argc, char* args[])
+int main( int argc, char* args[] )
 {
-	SDL_Window* window = NULL;
+  SDL_Window *window = NULL;
+  bool Running = false;
+  SDL_Event event;
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("Could not init video: %s\n", SDL_GetError);
+  }
+  else {
+    window = SDL_CreateWindow("SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    Running = true;
+  }
+  while(Running == true) {
+    while(SDL_PollEvent(&event) != 0) {
+      if(event.type == SDL_QUIT) {
+        Running = false;
+      }
+    }
+    SDL_UpdateWindowSurface(window);
+  }
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
-	SDL_Surface* screenSurface = NULL;
-
-	if( SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf( "SDL init error.\nSDL_Error: %s\n", SDL_GetError() );
-	}
-	else {
-		window = SDL_CreateWindow("SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN );
-		if(window == NULL) {
-			printf("Window couldn't be created.\nSDL_Error: %s\n", SDL_GetError());
-		}
-		else {
-			screenSurface = SDL_GetWindowSurface(window);
-
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			SDL_UpdateWindowSurface( window );
-
-            //Keep window up
-            SDL_Event event;
-            bool quit = false;
-            while( quit == false ){
-                while( SDL_PollEvent( &event ) ){
-                    if( event.type == SDL_QUIT ) quit = true;
-		}
-            		}
-		}
-	}
-
-	SDL_DestroyWindow( window );
-
-	SDL_Quit();
-
-	return 0;
+  return 0;
 }
