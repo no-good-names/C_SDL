67912127c01afe7c2164df2b2bbd5b2a763e36c8
i-dont-have-any-good-names <67664504+i-dont-have-any-good-names@users.noreply.github.com>
2023-03-13 07:46:29 -0500

    Update main.c
    not tested

--- main.c
+++ main.c
@@ -3,28 +3,26 @@
 #include <stdio.h>
 #include <SDL_image.h>
 
-#define TRUE 0
-#define FALSE 1
-
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 
 int main( int argc, char* args[] )
 {
   SDL_Window *window = NULL;
-  int running = FALSE;
+  int running = 0;
   SDL_Event event;
   if(SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("Could not init video: %s\n", SDL_GetError);
+    return 1;
   }
   else {
-    window = SDL_CreateWindow("SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    running = TRUE;
+    running = 1;
   }
-  while(running == TRUE) {
+  window = SDL_CreateWindow("SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+  while(running == 1) {
     while(SDL_PollEvent(&event) != 0) {
       if(event.type == SDL_QUIT) {
-        running = FALSE;
+        running = 0;
       }
     }
     SDL_UpdateWindowSurface(window);
