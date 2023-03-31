47f73b10bd3f51fe92095f2867797993a8b59121
Bad name <67664504+no-good-names@users.noreply.github.com>
2023-03-31 04:00:17 +0000

    opt

--- src/main.c
+++ src/main.c
@@ -6,40 +6,32 @@
 #define TRUE 1
 #define FALSE 0
 
-SDL_Event e;
+SDL_Event event;
 
 const int WINDOW_WIDTH = 1640;
 const int WINDOW_HEIGHT = 1480;
 
-void eventHandler (int running) {
-    if(e.type == SDL_QUIT) {
-        running = FALSE;
-    }
-    if(e.type == SDL_KEYUP) {
-		switch(e.key.keysym.sym) {
-		    case SDLK_w:
-		        printf("up\n");
-				break;
-			case SDLK_s:
-				printf("down\n");
-				break;
-			case SDLK_a:
-				printf("left\n");
-				break;
-			case SDLK_d:
-				printf("right\n");
-				break;
-		}
+void eventHandler (int isRunning) {
+    while(SDL_PollEvent(&event) != FALSE) {
+        if(event.type == SDL_KEYDOWN) {
+            switch(event.key.keysym.sym) {
+            case SDL_QUIT:
+                isRunning = FALSE;
+                break;
+            case SDLK_ESCAPE:
+                isRunning = FALSE;
+            }
+        }
     }
 }
 
 int main( int argc, char* args[] )
 {
-    int running = FALSE;
+    int isRunning = FALSE;
     SDL_Window *window = NULL;
     SDL_Surface *winSurface = NULL;
 
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_EVERYTHING);
 
     window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
@@ -47,17 +39,13 @@ int main( int argc, char* args[] )
         printf("Window couldn't init\n");
         SDL_Quit();
     }
-    else {
-        winSurface = SDL_GetWindowSurface(window);
-        SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 0xFF, 0xFF, 0xFF));
-        SDL_UpdateWindowSurface(window);
-        running = TRUE;
-    }
 
-    while(running == TRUE) {
-        while(SDL_PollEvent(&e) != 0) {
-            eventHandler(running);
-        }
+    winSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+
+    while(isRunning) {
+        eventHandler(isRunning);
         SDL_UpdateWindowSurface(window);
     }
     SDL_DestroyWindow(window);
