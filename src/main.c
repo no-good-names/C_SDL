f581f620e0aa9fbcff69535b60867c055e2bbc27
Bad name <67664504+no-good-names@users.noreply.github.com>
2023-04-08 01:38:29 -0500

    Update main.c
    fixed 👍

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdio.h>
 
 #define elif else if // I like python elif
 
@@ -8,52 +8,52 @@
 
 SDL_Event event;
 
-const int WINDOW_WIDTH = 1640;
-const int WINDOW_HEIGHT = 1480;
+const int WINDOW_WIDTH = 400;
+const int WINDOW_HEIGHT = 300;
 
-void eventHandler (int isRunning) {
-    while(SDL_PollEvent(&event) != FALSE) {
-    case SDL_QUIT:
-        isRunning = FALSE;
-        break;
-        if(event.type == SDL_KEYDOWN) {
-            switch(event.key.keysym.sym) {
-            case SDLK_ESCAPE:
-                isRunning = FALSE;
-                break;
-            case SDLK_1:
-                printf("Input test: 1\n");
-                break;
-            }
-        }
-    }
+void eventHandler(int isRunning) {
+	while (SDL_PollEvent(&event) != FALSE) {
+		if (event.type == SDL_QUIT) {
+			isRunning = false;
+		}
+		if (event.type == SDL_KEYDOWN) {
+			switch (event.key.keysym.sym) {
+			case SDLK_ESCAPE:
+				isRunning = FALSE;
+				break;
+			case SDLK_1:
+				printf("Input test: 1\n");
+				break;
+			}
+		}
+	}
 }
 
-int main( int argc, char* args[] )
-{
-    int isRunning = FALSE;
-    SDL_Window *window = NULL;
-    SDL_Surface *winSurface = NULL;
+int main(int argc, char *args[]) {
+	int isRunning = FALSE;
+	SDL_Window *window = NULL;
+	SDL_Surface *winSurface = NULL;
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-    window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
-    if(window == NULL) {
-        printf("Window couldn't init\n");
-        SDL_Quit();
-    }
+	if (window == NULL) {
+		printf("Window couldn't init\n: %s", SDL_GetError);
+		SDL_Quit();
+	} else {
+		isRunning = TRUE;
+	}
 
-    winSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
+	winSurface = SDL_GetWindowSurface(window);
+	SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 0xFF, 0xFF, 0xFF));
 
-    while(isRunning) {
-        eventHandler(isRunning);
-        SDL_UpdateWindowSurface(window);
-    }
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	while (isRunning) {
+		eventHandler(isRunning);
+		SDL_UpdateWindowSurface(window);
+	}
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
-    return 0;
+	return 0;
 }
