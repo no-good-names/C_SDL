91d201feb8959c53e4d9f0ceb60adaa3031c2e1d
Bad name <67664504+no-good-names@users.noreply.github.com>
2023-05-25 07:56:25 -0500

    Create maindef.h

---
+++ src/maindef.h
@@ -0,0 +1,14 @@
+#ifndef __MAINDEF__
+#define __MAINDEF__
+
+#include <stdio.h>
+#include "maintype.h"
+
+#ifndef __BYTEBOOL__
+#define __BYTEBOOL__
+
+typedef enum {false, true} boolean;
+
+#endif
+
+#endif
